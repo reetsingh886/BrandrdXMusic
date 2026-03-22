8159f8eba102a5fc69e38959c5a5ab93b343c022
ᯓ‌𐌌།⃪᱂ ⚠️𝑻𝒉𝒂𝒌𝒖𝒓𓂃 <reetsingh274@gmail.com>
2026-03-23 03:01:43 +0530

    Update alive.py

--- BrandrdXMusic/plugins/tools/alive.py
+++ BrandrdXMusic/plugins/tools/alive.py
@@ -15,15 +15,15 @@ async def start(client: Client, message: Message):
             [
                [
             InlineKeyboardButton(
-                text="☆ ʙʀᴀɴᴅᴇᴅ 💗 ", url=f"https://t.me/BRANDRDKING82"
+                text="☆ ᴛʜᴀᴋᴜʀ 💗 ", url=f"https://t.me/iamthakur007"
             ),
             InlineKeyboardButton(
-                text="☆ ꜱᴜᴘᴘᴏʀᴛ 💗", url=f"https://t.me/BRANDED_WORLD"
+                text="☆ ꜱᴜᴘᴘᴏʀᴛ 💗", url=f"https://t.me/BotsSupport_36"
             ),
         ],
                 [
             InlineKeyboardButton(
-                text="☆ ᴄʜᴀɴɴᴇʟ💗", url=f"https://t.me/BRANDRD_BOT"
+                text="☆ ᴄʜᴀɴɴᴇʟ💗", url=f"https://t.me/BOTxBOOSTER"
             ),
                 ],
                 [
