88f2dc540325860b62ef286cbcc231b7434a6fd7
ᯓ‌𐌌།⃪᱂ ⚠️𝑻𝒉𝒂𝒌𝒖𝒓𓂃 <reetsingh274@gmail.com>
2026-03-23 03:14:57 +0530

    Update help.py

--- BrandrdXMusic/plugins/bot/help.py
+++ BrandrdXMusic/plugins/bot/help.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message, InlineKeyboardButton
+from pyrogram.errors import MessageNotModified
 
 from BrandrdXMusic import app
 from BrandrdXMusic.utils import help_pannel
@@ -29,9 +30,13 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_, True)
-        await update.edit_message_text(
-            _["help_1"].format(SUPPORT_CHAT), reply_markup=keyboard
-        )
+
+        try:
+            await update.edit_message_text(
+                _["help_1"].format(SUPPORT_CHAT), reply_markup=keyboard
+            )
+        except MessageNotModified:
+            pass
     else:
         try:
             await update.delete()
@@ -43,7 +48,9 @@ async def helper_private(
         
         await update.reply_video(
             video="https://graph.org/file/84d30d4fd04570c0e0256.mp4",
-            caption=_["help_1"].format(SUPPORT_CHAT), reply_markup=keyboard)
+            caption=_["help_1"].format(SUPPORT_CHAT),
+            reply_markup=keyboard
+        )
 
 
 @app.on_message(filters.command(["help"]) & filters.group & ~BANNED_USERS)
@@ -52,8 +59,10 @@ async def help_com_group(client, message: Message, _):
     keyboard = private_help_panel(_)
     await message.reply_video(
         video="https://te.legra.ph/file/51293513e6af319726fe7.mp4",
-        caption=_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard)
+        caption=_["help_2"],
+        reply_markup=InlineKeyboardMarkup(keyboard)
     )
+
 
 @app.on_callback_query(filters.regex("help_callback") & ~BANNED_USERS)
 @languageCB
@@ -61,59 +70,90 @@ async def helper_cb(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     cb = callback_data.split(None, 1)[1]
     keyboard = help_back_markup(_)
-    if cb == "hb1":
-        await CallbackQuery.edit_message_text(helpers.HELP_1, reply_markup=keyboard)
-    elif cb == "hb2":
-        await CallbackQuery.edit_message_text(helpers.HELP_2, reply_markup=keyboard)
-    elif cb == "hb3":
-        await CallbackQuery.edit_message_text(helpers.HELP_3, reply_markup=keyboard)
-    elif cb == "hb4":
-        await CallbackQuery.edit_message_text(helpers.HELP_4, reply_markup=keyboard)
-    elif cb == "hb5":
-        await CallbackQuery.edit_message_text(helpers.HELP_5, reply_markup=keyboard)
-    elif cb == "hb6":
-        await CallbackQuery.edit_message_text(helpers.HELP_6, reply_markup=keyboard)
-    elif cb == "hb7":
-        await CallbackQuery.edit_message_text(helpers.HELP_7, reply_markup=keyboard)
-    elif cb == "hb8":
-        await CallbackQuery.edit_message_text(helpers.HELP_8, reply_markup=keyboard)
-    elif cb == "hb9":
-        await CallbackQuery.edit_message_text(helpers.HELP_9, reply_markup=keyboard)
-    elif cb == "hb10":
-        await CallbackQuery.edit_message_text(helpers.HELP_10, reply_markup=keyboard)
-    elif cb == "hb11":
-        await CallbackQuery.edit_message_text(helpers.HELP_11, reply_markup=keyboard)
-    elif cb == "hb12":
-        await CallbackQuery.edit_message_text(helpers.HELP_12, reply_markup=keyboard)
-    elif cb == "hb13":
-        await CallbackQuery.edit_message_text(helpers.HELP_13, reply_markup=keyboard)
-    elif cb == "hb14":
-        await CallbackQuery.edit_message_text(helpers.HELP_14, reply_markup=keyboard)
-    elif cb == "hb15":
-        await CallbackQuery.edit_message_text(helpers.HELP_15, reply_markup=keyboard)
-    
 
+    try:
+        if cb == "hb1":
+            await CallbackQuery.edit_message_text(helpers.HELP_1, reply_markup=keyboard)
+        elif cb == "hb2":
+            await CallbackQuery.edit_message_text(helpers.HELP_2, reply_markup=keyboard)
+        elif cb == "hb3":
+            await CallbackQuery.edit_message_text(helpers.HELP_3, reply_markup=keyboard)
+        elif cb == "hb4":
+            await CallbackQuery.edit_message_text(helpers.HELP_4, reply_markup=keyboard)
+        elif cb == "hb5":
+            await CallbackQuery.edit_message_text(helpers.HELP_5, reply_markup=keyboard)
+        elif cb == "hb6":
+            await CallbackQuery.edit_message_text(helpers.HELP_6, reply_markup=keyboard)
+        elif cb == "hb7":
+            await CallbackQuery.edit_message_text(helpers.HELP_7, reply_markup=keyboard)
+        elif cb == "hb8":
+            await CallbackQuery.edit_message_text(helpers.HELP_8, reply_markup=keyboard)
+        elif cb == "hb9":
+            await CallbackQuery.edit_message_text(helpers.HELP_9, reply_markup=keyboard)
+        elif cb == "hb10":
+            await CallbackQuery.edit_message_text(helpers.HELP_10, reply_markup=keyboard)
+        elif cb == "hb11":
+            await CallbackQuery.edit_message_text(helpers.HELP_11, reply_markup=keyboard)
+        elif cb == "hb12":
+            await CallbackQuery.edit_message_text(helpers.HELP_12, reply_markup=keyboard)
+        elif cb == "hb13":
+            await CallbackQuery.edit_message_text(helpers.HELP_13, reply_markup=keyboard)
+        elif cb == "hb14":
+            await CallbackQuery.edit_message_text(helpers.HELP_14, reply_markup=keyboard)
+        elif cb == "hb15":
+            await CallbackQuery.edit_message_text(helpers.HELP_15, reply_markup=keyboard)
+    except MessageNotModified:
+        pass
+
+
+# ✅ FIXED FUNCTION NAME + ERROR HANDLE
 @app.on_callback_query(filters.regex("mbot_cb") & ~BANNED_USERS)
-async def helper_cb(client, CallbackQuery):
-    await CallbackQuery.edit_message_text(Helper.HELP_M, reply_markup=InlineKeyboardMarkup(BUTTONS.MBUTTON))
+async def helper_mbot(client, CallbackQuery):
+    try:
+        await CallbackQuery.edit_message_text(
+            Helper.HELP_M,
+            reply_markup=InlineKeyboardMarkup(BUTTONS.MBUTTON)
+        )
+    except MessageNotModified:
+        pass
 
 
 @app.on_callback_query(filters.regex('managebot123'))
 async def on_back_button(client, CallbackQuery):
-    callback_data = CallbackQuery.data.strip()
-    cb = callback_data.split(None, 1)[1]
-    keyboard = help_pannel(_, True)
-    if cb == "settings_back_helper":
-        await CallbackQuery.edit_message_text(
-            _["help_1"].format(SUPPORT_CHAT), reply_markup=keyboard
-        )
+    try:
+        callback_data = CallbackQuery.data.strip()
+        cb = callback_data.split(None, 1)[1]
+        language = await get_lang(CallbackQuery.message.chat.id)
+        _ = get_string(language)
+        keyboard = help_pannel(_, True)
+
+        if cb == "settings_back_helper":
+            await CallbackQuery.edit_message_text(
+                _["help_1"].format(SUPPORT_CHAT),
+                reply_markup=keyboard
+            )
+    except MessageNotModified:
+        pass
+
 
 @app.on_callback_query(filters.regex('mplus'))      
 async def mb_plugin_button(client, CallbackQuery):
     callback_data = CallbackQuery.data.strip()
     cb = callback_data.split(None, 1)[1]
-    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("ʙᴀᴄᴋ", callback_data=f"mbot_cb")]])
-    if cb == "Okieeeeee":
-        await CallbackQuery.edit_message_text(f"`something errors`",reply_markup=keyboard,parse_mode=enums.ParseMode.MARKDOWN)
-    else:
-        await CallbackQuery.edit_message_text(getattr(Helper, cb), reply_markup=keyboard)
+    keyboard = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("ʙᴀᴄᴋ", callback_data="mbot_cb")]]
+    )
+
+    try:
+        if cb == "Okieeeeee":
+            await CallbackQuery.edit_message_text(
+                "`something errors`",
+                reply_markup=keyboard
+            )
+        else:
+            await CallbackQuery.edit_message_text(
+                "Something went wrong",
+                reply_markup=keyboard
+            )
+    except MessageNotModified:
+        pass
